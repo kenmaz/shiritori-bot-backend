6bdb622bf7fc2fa970c36e13cae4e9055b6ee7f0
Kentaro Matsumae <kentaro.matsumae@dena.com>
2016-04-09 05:04:33 +0900

    fix2

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,11 +4,29 @@ class Word < ActiveRecord::Base
 
   # @return [:code => (:ok|:out|:invalid), :msg => ".."]
   def self.check(text)
+    kana = text.tr('ァ-ン','ぁ-ん')
+    unless /\p{hiragana}/.match(kana)
+      return {:code => :invalid, :msg => "ひらがなorカタカナでお願いします"}
+    end
+
+    if text.end_with?("ん")
+      return {:code => :out, :msg => "「ん」で終わってる！あなたの負け！"}
+    end
+
+    if word = Word.where(kana: kana).take
+      word.cnt += 1
+    else
+      word = Word.new(kana: kana, cnt: 1)
+    end
+    word.save!
+
     return {:code => :ok}
   end
 
   def self.answer(text)
     if text
+      kana = text.tr('ァ-ン','ぁ-ん')
+      ch = kana[-1]
       return "なにか"
     else
       return "オレオ"
